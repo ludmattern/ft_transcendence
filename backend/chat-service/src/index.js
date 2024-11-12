23f221770e7f6318cf658504b9f30b0649cd0bf5
Felix Prevot <fprevot@z2r3p7.42lyon.fr>
2024-11-12 03:19:28 +0100

    add every server for every service

--- backend/chat-service/src/index.js
+++ backend/chat-service/src/index.js
@@ -1,10 +1,9 @@
-// backend/user-service/src/index.js
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3003;
 
 app.get('/', (req, res) => {
-  res.send("USER");
+  res.send("CHAT");
 });
 
 app.listen(PORT, () => {
