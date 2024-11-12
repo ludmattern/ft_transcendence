94941e4dd8420c5598bc04b90f9dc1ffad9c9c86
Felix Prevot <fprevot@z2r3p7.42lyon.fr>
2024-11-12 02:35:16 +0100

    add css and tst

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 // frontend/src/App.js
 import React from 'react';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Hello, World!</h1>
+      <p>Welcome to my React application. Let's make it beautiful!</p>
     </div>
   );
 }
