f0dcbc8e108fd931786be2042cf6e127f5c0b5a2
LossaLos <felixprevot@gmail.com>
2024-11-13 08:24:31 +0100

    replace email by username on login

--- backend/auth-service/src/index.js
+++ backend/auth-service/src/index.js
@@ -85,10 +85,10 @@ app.post('/register', async (req, res) => {
 });
 
 app.post('/login', async (req, res) => {
-	const { email, password } = req.body;
+	const { username, password } = req.body;
   
 	try {
-	  const userResult = await pool.query('SELECT * FROM users WHERE email = $1', [email]);
+	  const userResult = await pool.query('SELECT * FROM users WHERE username = $1', [username]);
 	  const user = userResult.rows[0];
 	  if (!user) return res.status(400).json({ message: 'Utilisateur non trouvé' });
   
