7657d4185d3c584deb088b7c245083061bfc2e2f
LossaLos <felixprevot@gmail.com>
2024-12-05 14:26:12 +0100

    new

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,13 +4,9 @@ import { LeftSideWindow } from "./components/leftSideWindow.js";
 import { RightSideWindow } from "./components/rightSideWindow.js";
 import { loadComponent } from "./utils/dom_utils.js";
 import { PongMenu } from "./components/pongMenu.js";
-import { game2 } from "./components/game2.js";
 import { midScreen } from "./components/midScreen.js";
-import { initializeMenuElements } from "./App.js";
-import { initm2 } from "./App.js";
 import { HelmetSVG } from "./components/HelmetSVG.js";
 import { HUDSVG } from "./components/HUDSVG.js";
-import { Menu } from "./components/pongMenu.js"; 
 
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -20,9 +16,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
   loadComponent("right-window-placeholder", RightSideWindow, "rightsidewindow", () => {});
 
-  loadComponent("mid-placeholder", midScreen, "pongmenu", () => { initm2(); });
+  loadComponent("mid-placeholder", midScreen, "pongmenu", () => {  });
 
-  loadComponent("pongmenu-placeholder", PongMenu, "pongmenu", () => { initializeMenuElements(); });
+  loadComponent("pongmenu-placeholder", PongMenu, "pongmenu", () => {  });
 
   loadComponent("helmet-svg-placeholder", HelmetSVG, "", () => {});
 
---
+++ frontend/src/pages/pong/pongmenu.js
@@ -0,0 +1,6 @@
+
+import { initm2 } from "../../App.js";
+import { initializeMenuElements } from "../../App.js";
+
+initm2();
+initializeMenuElements();
