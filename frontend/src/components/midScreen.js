94db7bbbff6efb4f81c6cc024ca78a350fa84ff3
LossaLos <felixprevot@gmail.com>
2024-12-05 15:54:45 +0100

    add everything with mini react

--- frontend/src/components/midScreen.js
+++ frontend/src/components/midScreen.js
@@ -1,12 +1,25 @@
 
 import { createElement } from "../utils/mini_react.js";
-
+import { BackButton } from "./pongMenu.js";
 
 
 export function midScreen() {
   return createElement(
     "div",
     { className: `menu2`, id: "gameScreen" },
-    "dasda" 
+    createElement(
+      "div",
+      {className :"row mt-3"},
+      createElement("div", { className: "col-12" }, BackButton())
+
+    ),
+    createElement(
+      "div",
+      {className : "wait2"},
+      createElement(
+        "img",
+        {src : "../src/assets/img/image-removebg-preview (1).png"},
+      )
+    )
   );
 }
