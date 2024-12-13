5bc99fd993cf3e72b3d2fd263eebe37d9615a3e2
ludmattern <github.rfgrg@passmail.net>
2024-12-13 02:10:51 +0100

    added the blur effect on links

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -15,32 +15,25 @@ import { HelmetSVG } from "./components/HelmetSVG.js";
 import { HUDSVG } from "./components/HUDSVG.js";
 import { game2 } from "./components/game2.js";
 
-// Fonction principale pour initialiser le comportement
 async function initializeApp() {
-    // Charger les SVG de base (toujours affichés)
     loadSVGComponents();
 
-    // Vérifie si l'utilisateur est authentifié
     const isAuthenticated = await isClientAuthenticated();
 
     if (!isAuthenticated) {
-        // Si non authentifié, afficher uniquement la page de connexion
         loadComponent("#central-window", LoginForm, "loginForm", () => {
             console.debug("LoginForm loaded as user is not authenticated.");
         });
-        return; // Ne pas charger le reste des composants
+        return;
     }
 	
-    // Si authentifié, charger les autres composants
 	document.getElementById("waiting-screen-effect").classList.add("d-none");
 	document.getElementById("blur-screen-effect").classList.add("d-none");
     loadAuthenticatedComponents();
 
-    // Ajouter les gestionnaires d'événements
     setupEventListeners();
 }
 
-// Charger les SVG de base
 function loadSVGComponents() {
     document.addEventListener("DOMContentLoaded", () => {
         loadComponent("helmet-svg-placeholder", HelmetSVG, "", () => {});
@@ -48,7 +41,6 @@ function loadSVGComponents() {
     });
 }
 
-// Charger les composants pour les utilisateurs authentifiés
 function loadAuthenticatedComponents() {
     loadComponent("header-placeholder", Header, "", () => {});
     loadComponent("left-window-placeholder", LeftSideWindow, "leftsidewindow", () => {});
@@ -58,23 +50,27 @@ function loadAuthenticatedComponents() {
     loadComponent("pongmenu-placeholder", PongMenu, "pongmenu", () => {});
 }
 
-// Ajoute les gestionnaires d'événements
 function setupEventListeners() {
     document.getElementById("profile-link").addEventListener("click", function (e) {
         e.preventDefault();
         loadComponent("#central-window", ProfileForm, "", () => {
             console.debug("ProfileForm loaded on click.");
         });
+		document.getElementById("blur-screen-effect").classList.remove("d-none");
     });
 
     document.getElementById("pong-link").addEventListener("click", function (e) {
         e.preventDefault();
         switchwindow("pong");
+        loadComponent("#central-window", null, "", () => {});
+		document.getElementById("blur-screen-effect").classList.add("d-none");
     });
 
     document.getElementById("race-link").addEventListener("click", function (e) {
         e.preventDefault();
         switchwindow("race");
+        loadComponent("#central-window", null, "", () => {});
+		document.getElementById("blur-screen-effect").classList.add("d-none");
     });
 
     document.getElementById("social-link").addEventListener("click", function (e) {
@@ -82,15 +78,16 @@ function setupEventListeners() {
         loadComponent("#central-window", SocialForm, "socialForm", () => {
             console.debug("SocialForm loaded on click.");
         });
+		document.getElementById("blur-screen-effect").classList.remove("d-none");
     });
 
     document
-        .getElementById("settings-link")
-        .addEventListener("click", function (e) {
+        .getElementById("settings-link").addEventListener("click", function (e) {
             e.preventDefault();
             loadComponent("#central-window", SettingsForm, "settingsForm", () => {
                 console.debug("SettingsForm loaded on click.");
             });
+			document.getElementById("blur-screen-effect").classList.remove("d-none");
         });
 
     document.getElementById("logout-link").addEventListener("click", function (e) {
@@ -98,14 +95,15 @@ function setupEventListeners() {
         loadComponent("#central-window", LogoutForm, "", () => {
             console.debug("LogoutForm loaded on click.");
         });
+		document.getElementById("blur-screen-effect").classList.remove("d-none");
     });
 
     document.getElementById("home-link").addEventListener("click", function (e) {
         e.preventDefault();
         switchwindow(null);
         loadComponent("#central-window", null, "", () => {});
+		document.getElementById("blur-screen-effect").classList.add("d-none");
     });
 }
 
-// Appeler la fonction d'initialisation
 initializeApp();
