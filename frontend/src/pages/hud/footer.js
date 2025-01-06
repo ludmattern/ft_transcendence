e893ffad06c85beb2a9681c6be9c4989f5211a1d
ludmattern <github.rfgrg@passmail.net>
2025-01-06 19:45:56 +0100

    removed console logs into the footer events

--- frontend/src/pages/hud/footer.js
+++ frontend/src/pages/hud/footer.js
@@ -20,25 +20,19 @@ function positionPoints(offset = 180) {
 }
 
 function handleResize() {
-	console.log("resize");
 	positionPoints();
 }
 
 window.addEventListener("resize", handleResize);
 
 function headIsMoving(cameraRotationEvent) {
-	console.log("camera orientation y:", cameraRotationEvent);
 	const curve = 110 * (1.2 / (window.innerWidth / 3840));
 	const percent = (cameraRotationEvent + 1) / 2;
 	const offset = percent * 360 * rotationRatio + 180;
 	positionPoints(offset);
-	console.log("offset", offset);
 }
 
-console.log("compass points", points);
-
 addCameraRotationListener(headIsMoving);
-
 positionPoints();
 
 document.getElementById("free-view").addEventListener("click", toggleFreeView);
