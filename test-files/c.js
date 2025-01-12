1ca602b28c4ee685fe819cd885548b6a962d1b36
ludmattern <github.rfgrg@passmail.net>
2025-01-12 19:09:43 +0100

    test reflection on left screen

--- test-files/c.js
+++
@@ -1,9 +0,0 @@
-function joinTournament() {
-    document.getElementById('tournamentList').classList.add('d-none');
-    document.getElementById('waitingRoom').classList.remove('d-none');
-  }
-
-  function leaveTournament() {
-    document.getElementById('waitingRoom').classList.add('d-none');
-    document.getElementById('tournamentList').classList.remove('d-none');
-  }
