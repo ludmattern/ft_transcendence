8a8d4766802f0b5bc3bade2a021810ba45e6ffbb
LossaLos <felixprevot@gmail.com>
2025-01-13 16:01:43 +0100

    move token from localstorage to sessionStorage with cookies

--- backend/auth-service/transcendance/urls.py
+++ backend/auth-service/transcendance/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path
-from users.views import login_view, register_user, protected_view, check_auth_view
+from users.views import login_view, register_user, protected_view, check_auth_view, logout_view
 
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('register/', register_user, name='register_user'),
     path('protected/', protected_view, name='protected_view'),
     path('check-auth/', check_auth_view, name='check_auth'),
+    path('logout/', logout_view, name='logout'),
+
 ]
--- frontend/src/services/auth.js
+++ frontend/src/services/auth.js
@@ -2,39 +2,27 @@ import { handleRoute } from "/src/services/router.js";
 
 
 export async function isClientAuthenticated() {
-  const token = localStorage.getItem("authToken");
-  if (!token) {
-    console.error("No token found");
-    return false;
-  }
 
   try {
     const response = await fetch("/api/auth-service/check-auth/", {
       method: "GET",
-      headers: {
-        "Authorization": `Bearer ${token}`
-      }
+      credentials: "include"
     });
 
     if (response.status === 401) 
     {
       const errData = await response.json().catch(() => ({}));
       console.warn(errData.message || "Unauthorized");
-      localStorage.removeItem("authToken");
       return false;
     }
 
     const data = await response.json();
     if (!data.success) {
       console.warn(data.message);
-      localStorage.removeItem("authToken");
       return false;
     }
-
- 
     if (data.new_access_token) {
       console.log("Got new token from backend");
-      localStorage.setItem("authToken", data.new_access_token);
     }
 
     return true;
@@ -73,59 +61,35 @@ export async function ensureAuthenticated(callback, allowUnauthenticated = false
 }
 
 export async function logoutUser() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      const success = true;
-      if (success) {
-        localStorage.removeItem("authToken");
-        resolve();
-      } else {
-        reject(new Error("Logout failed!"));
-      }
-    }, 500);
-  });
-}
-
-// async function getProtectedData() {
-//   const token = localStorage.getItem("authToken");
-//   const response = await fetch("/api/auth-service/protected/", {
-//     method: "GET",
-//     headers: {
-//       "Authorization": `Bearer ${token}`
-//     }
-//   });
-//   const data = await response.json();
-//   console.log(data);
-// }
-
-// function startTokenRefreshInterval() {
-//   setInterval(async () => {
-//     const isAuthenticated = await isClientAuthenticated();
-//     if (!isAuthenticated) {
-//       console.warn("User is no longer authenticated. Redirecting to login...");
-//       handleRoute("/login");
-//     }
-//   }, 60000); // Vérifie toutes les 60 secondes
-// }
-
-export async function loginUser(username, password) {
   try {
-    const response = await fetch("/api/auth-service/logindb/", {
+    const response = await fetch("/api/auth-service/logout/", {
       method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ username, password }),
+      credentials: "include",
     });
-    const data = await response.json();
-    if (data.success) 
-    {
-      localStorage.setItem("authToken", data.access_token);
-      localStorage.setItem("refreshToken", data.refresh_token);
-      console.log("Login successful!");
+
+    if (response.ok) {
+
+      console.log("Logout successful!");
     } else {
-      console.log("Login failed:", data.message);
+      console.error("Logout failed:", await response.text());
     }
   } catch (err) {
-    console.error("Error:", err);
+    console.error("Error during logout:", err);
+  }
+}
+
+export async function loginUser(username, password) {
+  const response = await fetch("/api/auth-service/logindb/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ username, password }),
+    credentials: "include"
+  });
+  const data = await response.json();
+  if (data.success) {
+    console.log("Login successful (cookie set)!");
+  } else {
+    console.log("Login failed:", data.message);
   }
 }
 
@@ -155,17 +119,3 @@ export async function registerUser(id, password, email) {
     alert("An error occurred. Please try again later.");
   }
 }
-
-
-
-/*
-
-12s -> pas de refresh -> 30s
-
-22s -> refresh -> 52s
-
-35s -> pas de refresh -> 52s
-
-45s-> refresh ->1m15s
-
-*/
