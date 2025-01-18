285beefb82b3d61f3d7f1bdb8cfbfe15f768668f
ludmattern <github.rfgrg@passmail.net>
2025-01-18 18:19:18 +0100

    minor style changes

--- frontend/src/components/hud/sideWindow/left/leftSideWindow.js
+++ frontend/src/components/hud/sideWindow/left/leftSideWindow.js
@@ -80,7 +80,9 @@ function createNavItem(label, active = false) {
     <li class="nav-item">
       <span class="nav-link ${
         active ? "active" : ""
-      }" data-tab="${label.toLowerCase()}">${label}</span>
+      }" data-tab="${label.toLowerCase()}">
+	  	<a href="#" data-tab="${label.toLowerCase()}">${label}</a>
+	  </span>
     </li>
   `;
 }
