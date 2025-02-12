dc49ae3e1030e060fca903c347ed251dd7731301
ludmattern <github.rfgrg@passmail.net>
2025-02-12 16:21:28 +0100

    loader

--- frontend/src/components/hud/general/headerIngame.js
+++ frontend/src/components/hud/general/headerIngame.js
@@ -6,35 +6,12 @@ export const headerIngame = createComponent({
   tag: "headerIngame",
 
   render: () => `
-      <div class="row">
+      <div class="row mb-5">
         <div class="col-12 text-center">
           <h1 class="hud-title interactive">
             <a>PONG</a>
           </h1>
         </div>
-      </div>
-      <div class="row">
-        <nav class="col-12 d-flex justify-content-center">
-          <ul class="nav">
-            <!-- Menu gauche -->
-            <span class="left-menu">
-            </span>
-            <!-- Jeux (Menu central) -->
-            <li class="nav-item first-game">
-              <span class="nav-link text-white">
-                <a href="/pong" id="pong-link">p1 : give up</a>
-              </span>
-            </li>
-            <li class="nav-item second-game">
-              <span class="nav-link text-white">
-                <a href="/race" id="race-link">p2 : give up</a>
-              </span>
-            </li>
-            <!-- Menu droit -->
-            <span class="right-menu">
-            </span>
-          </ul>
-        </nav>
       </div>
   `,
 
@@ -47,20 +24,3 @@ export const headerIngame = createComponent({
 		startAnimation(navItems, "light-animation", 1000);
 	}
 });
-
-/**
- * Crée un élément de navigation (<li>) avec un lien (<a>)
- *
- * @param {string} text - Le texte du lien
- * @param {string} id - L'ID de l'élément <a>
- * @returns {string} - HTML du menu de navigation
- */
-function createNavItem(text, id = "") {
-  return `
-    <li class="nav-item">
-      <span class="nav-link text-white">
-        <a href="/${text}" id="${id}">${text}</a>
-      </span>
-    </li>
-  `;
-}
