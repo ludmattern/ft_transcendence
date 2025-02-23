254f45300c954f7cc9605bebdac1b5d60564002b
LossaLos <felixprevot@gmail.com>
2025-02-23 15:13:00 +0100

    clear tournament code

--- backend/tournament-service/tournament/urls.py
+++ backend/tournament-service/tournament/urls.py
@@ -5,6 +5,4 @@ urlpatterns = [
     path("get_current_tournament/", get_current_tournament, name="get_current_tournament"),
     path("update_match_result/", update_match_result, name="update_match_result"),
     path("abandon_local_tournament/", abandon_local_tournament, name="abandon_local_tournament"),
-
-
 ]
--- frontend/src/services/tournamentHandler.js
+++ frontend/src/services/tournamentHandler.js
@@ -1,5 +1,54 @@
 import { ws } from '/src/services/socketManager.js';
 
+
+
+function parseGameId(gameId) {
+	const regex = /^tournLocal_(.+)_vs_tournLocal_(.+)_id_(.+)$/;
+	const match = gameId.match(regex);
+	if (match) {
+		const player1 = match[1];
+		const player2 = match[2];
+		const tournamentId = match[3];
+		return { player1, player2, tournamentId };
+	} else {
+		throw new Error('Invalid gameId format');
+	}
+}
+
+export function handleLocalTournamentGameEnding(data) {
+	try {
+		const { player1, player2, tournamentId } = parseGameId(data.game_id);
+		const payload = {
+			game_id: data.game_id,
+			tournament_id: tournamentId,
+			winner_id: data.winner_id,
+			loser_id: data.loser_id,
+			final_scores: data.final_scores,
+			type: 'game_over',
+			player1: player1,
+			player2: player2,
+		};
+
+		fetch('/api/tournament-service/update_match_result/', {
+			method: 'POST',
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify(payload),
+		})
+			.then((res) => res.json())
+			.then((result) => {
+				console.log('Match result updated:', result);
+			})
+			.catch((err) => {
+				console.error('Error updating match result:', err);
+			});
+	} catch (error) {
+		console.error('Error parsing gameId:', error);
+	}
+}
+
+
 export function createTournament(players) {
 	const organizerId = sessionStorage.getItem('userId');
 	const payload = {
