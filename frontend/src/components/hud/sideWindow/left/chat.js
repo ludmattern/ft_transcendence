f523355b3c2c6ab65ebbc8155d2b98d49f0fc3d8
ludmattern <github.rfgrg@passmail.net>
2025-03-20 13:49:05 +0100

    patch little error on chat message length

--- frontend/src/components/hud/sideWindow/left/chat.js
+++ frontend/src/components/hud/sideWindow/left/chat.js
@@ -5,7 +5,7 @@ import { createNotificationMessage } from '/src/components/hud/sideWindow/left/n
 
 const messageHistory = [];
 let historyIndex = -1;
-let draftMessage = "";
+let draftMessage = '';
 
 export async function setupChatInput() {
 	const container = document.querySelector('#l-tab-content-container');
@@ -15,15 +15,15 @@ export async function setupChatInput() {
 
 	if (!container.querySelector('#message-input-container')) {
 		const inputContainer = `
-          <div class="d-flex" 
-              style="flex-wrap: wrap; background: #ffffff07; position: absolute; width: 100%;" 
-              id="message-input-container">
-          <input type="text" id="message-input" placeholder="Enter your message..." 
-                  class="form-control w-50 me-2 p-3" 
-                  style="flex: auto; color: var(--content-color);" />
-          <button id="chat-send-button" class="btn btn-sm">Send</button>
-          </div>
-      `;
+			<div class="d-flex" 
+				style="flex-wrap: wrap; background: #ffffff07; position: absolute; width: 100%;" 
+				id="message-input-container">
+			<input type="text" id="message-input" placeholder="Enter your message..." 
+					class="form-control w-50 me-2 p-3" 
+					style="flex: auto; color: var(--content-color);" />
+			<button id="chat-send-button" class="btn btn-sm">Send</button>
+			</div>
+		`;
 		container.insertAdjacentHTML('beforeend', inputContainer);
 	}
 
@@ -53,42 +53,47 @@ export async function setupChatInput() {
 
 		messageHistory.push(message);
 		historyIndex = messageHistory.length;
-		draftMessage = "";
+		draftMessage = '';
 
 		ws.send(JSON.stringify(payload));
 	}
 
 	if (inputField) {
 		inputField.addEventListener('keydown', (event) => {
-			if (event.key === 'ArrowUp') {
-				if (historyIndex === messageHistory.length) {
-					draftMessage = inputField.value;
-				}
-				if (historyIndex > 0) {
-					historyIndex--;
-					inputField.value = messageHistory[historyIndex];
-				}
-				event.preventDefault();
-			} else if (event.key === 'ArrowDown') {
-				if (historyIndex < messageHistory.length - 1) {
-					historyIndex++;
-					inputField.value = messageHistory[historyIndex];
-				} else if (historyIndex === messageHistory.length - 1) {
-					historyIndex++;
-					inputField.value = draftMessage;
-				}
-				event.preventDefault();
-			} else if (event.key === 'Enter') {
-				event.preventDefault();
-				const trimmedMessage = inputField.value.trim();
-				if (trimmedMessage !== '' && trimmedMessage.length <= 150) {
-					sendMessage(trimmedMessage);
-					messageHistory.push(trimmedMessage); // Save only trimmed messages
-					historyIndex = messageHistory.length; // Reset index
-					inputField.value = '';
-				}
-				else
-					createNotificationMessage('Message is too long, 150 chars. max.', 2500, true);
+			switch (event.key) {
+				case 'ArrowUp':
+					if (historyIndex === messageHistory.length) {
+						draftMessage = inputField.value;
+					}
+					if (historyIndex > 0) {
+						historyIndex--;
+						inputField.value = messageHistory[historyIndex];
+					}
+					event.preventDefault();
+					break;
+				case 'ArrowDown':
+					if (historyIndex < messageHistory.length - 1) {
+						historyIndex++;
+						inputField.value = messageHistory[historyIndex];
+					} else if (historyIndex === messageHistory.length - 1) {
+						historyIndex++;
+						inputField.value = draftMessage;
+					}
+					event.preventDefault();
+					break;
+				case 'Enter':
+					event.preventDefault();
+					const trimmedMessage = inputField.value.trim();
+					if (trimmedMessage !== '' && trimmedMessage.length <= 150) {
+						sendMessage(trimmedMessage);
+						historyIndex = messageHistory.length; // Reset index
+						inputField.value = '';
+					} else if (trimmedMessage.length > 150) {
+						createNotificationMessage('Message is too long, 150 chars. max.', 2500, true);
+					}
+					break;
+				default:
+					break;
 			}
 		});
 	}
@@ -98,19 +103,15 @@ export async function setupChatInput() {
 			const trimmedMessage = inputField.value.trim();
 			if (trimmedMessage !== '' && trimmedMessage.length <= 150) {
 				sendMessage(trimmedMessage);
-				messageHistory.push(trimmedMessage); // Save only trimmed messages
 				historyIndex = messageHistory.length; // Reset index
 				inputField.value = '';
-			}
-			else
+			} else if (trimmedMessage.length > 150) {
 				createNotificationMessage('Message is too long, 150 chars. max.', 2500, true);
+			}
 		});
 	}
 }
 
-/**
- * Supprime la zone de saisie pour les onglets autres que "COMM".
- */
 export function removeChatInput() {
 	const inputContainer = document.getElementById('message-input-container');
 	if (inputContainer) {
